05c30bf13314f7b1f05e79bd7ff60b7611be5498
mohits8962 <108714331+mohits8962@users.noreply.github.com>
2022-09-05 20:15:34 +0530

    adding

--- src/controller/blogsController.js
+++ src/controller/blogsController.js
@@ -2,20 +2,25 @@ const authormodel = require('../model/authormodel');
 const blogsModel = require('../model/blogsmodel');
 
 const createBlogs = async function (req, res) {
-    let blogs = req.body
-    let condition = await authormodel.findById(blogs.authorId)
-    if(condition){
-        if(blogs.isPublished == true){
-            blogs.publishedAt = Date.now()
-            let savedData = await blogsModel.create(blogs)
-            res.status(201).send({ data: savedData })
+    try {
+        let blogs = req.body
+        let condition = await authormodel.findById(blogs.authorId)
+        if (condition) {
+            if (blogs.isPublished == true) {
+                blogs.publishedAt = Date.now()
+                let savedData = await blogsModel.create(blogs)
+                res.status(201).send({ data: savedData })
+            }
+            else if (blogs.isPublished == false) {
+                let savedData = await blogsModel.create(blogs)
+                res.status(201).send({ data: savedData })
+            }
+        } else {
+            res.status(400).send({ status: false, msg: "authorId is not present" })
         }
-        else if(blogs.isPublished == false){
-            let savedData = await blogsModel.create(blogs)
-            res.status(201).send({ data: savedData })
-        }
-    }else{
-        res.status(400).send({status:false, msg:"authorId is not present"})
+    } catch (err) {
+        console.log("this is the error:", err.message)
+        res.status(500).send({ msg: "error", error: err.message })
     }
 }
 
