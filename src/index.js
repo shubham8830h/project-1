4fc12c68a6b73b0e663e0b5402aa725fb8ddd077
Gouravpundir <pundirgourav0@gmail.com>
2022-09-05 16:35:30 +0530

    created files

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://mohits8962:m26u72h8@group-26-database.f1gfhiv.mongodb.net/mohits-8962?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
